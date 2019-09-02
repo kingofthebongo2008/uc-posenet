03e81e616ca4d426065b175b68d8928ed8dd8a64
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2019-09-02 07:45:05 +0300

    modified

--- src/inference/private/main.cpp
+++ src/inference/private/main.cpp
@@ -14,6 +14,7 @@
 #include <util/EGLWindow.h>
 #include <util/OSWindow.h>
 #include <util/Event.h>
+#include "util/shader_utils.h"
 #include <common/system_utils.h>
 
 class Win32Library : public angle::Library
@@ -45,72 +46,134 @@ public:
 	void* getNative() const override { return reinterpret_cast<void*>(mModule); }
 
 private:
+
 	HMODULE mModule = nullptr;
 };
 
-int32_t main(int32_t, char*[])
-{ 
-	EGLWindow* egl = EGLWindow::New(3, 1);
-	OSWindow* w = OSWindow::New();
+struct EGLWindowDeleter
+{
+	void operator()(void* d)
+	{
+		EGLWindow* p = reinterpret_cast<EGLWindow*>(d);
+		EGLWindow::Delete(&p);
+	}
+};
 
+struct OSWindowDeleter
+{
+	void operator()(void* d)
+	{
+		OSWindow* p = reinterpret_cast<OSWindow*>(d);
+		OSWindow::Delete(&p);
+	}
+};
+
+ConfigParameters make_config_parameters()
+{
 	ConfigParameters configParams;
 
-	configParams.redBits		= 8;
-	configParams.greenBits		= 8;
-	configParams.blueBits		= 8;
-	configParams.alphaBits		= 8;
-	configParams.depthBits		= 24;
-	configParams.stencilBits	= 8;
+	configParams.redBits = 8;
+	configParams.greenBits = 8;
+	configParams.blueBits = 8;
+	configParams.alphaBits = 8;
+	configParams.depthBits = 0;
+	configParams.stencilBits = 0;
 
+	return configParams;
+}
+
+EGLPlatformParameters make_platform_parameters()
+{
 	EGLPlatformParameters mPlatformParams;
 
 	mPlatformParams.majorVersion = 3;
 	mPlatformParams.minorVersion = 1;
-	
-	w->initialize("test", 1600, 900);
-	w->setVisible(true);
+
+	return mPlatformParams;
+}
+
+int32_t main(int32_t, char*[])
+{ 
+	std::unique_ptr<EGLWindow, EGLWindowDeleter> egl(EGLWindow::New(3, 1), EGLWindowDeleter());
+	std::unique_ptr<OSWindow, OSWindowDeleter> w(OSWindow::New(), OSWindowDeleter());
 
 	Win32Library library;
 
-	bool init	= egl->initializeGL(w, &library, mPlatformParams, configParams);
+	w->initialize("main window", 1600, 900);
 
-	egl->setSwapInterval(0);
-	egl->makeCurrent();
+	bool init	= egl->initializeGL(w.get(), &library, make_platform_parameters(), make_config_parameters());
 
-	bool running = true;
-	while (running)
+	constexpr char kVS[] = R"(attribute vec4 vPosition;
+void main()
+{
+    gl_Position = vPosition;
+})";
+
+	constexpr char kFS[] = R"(precision mediump float;
+void main()
+{
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+})";
+
+	GLuint mProgram = CompileProgram(kVS, kFS);
+	if (!mProgram)
 	{
-		// Clear events that the application did not process from this frame
-		Event event;
-		while (w->popEvent(&event))
-		{
-			// If the application did not catch a close event, close now
-			if (event.Type == Event::EVENT_CLOSED)
-			{
-				running = false;
-			}
-		}
-
-		if (!running)
-		{
-			break;
-		}
-
-		glViewport(0, 0, 1600, 900);
-		glClearColor(0.5, 0, 0, 0);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		egl->swap();
-		w->messageLoop();
-
-		//prevTime = elapsedTime;
+		return false;
 	}
 
-	EGLWindow::Delete(&egl);
-	w->destroy();
-	OSWindow::Delete(&w);
 
-    return 0;
+	if (init)
+	{
+		w->setVisible(true);
+
+		bool running = true;
+		while (running)
+		{
+			// Clear events that the application did not process from this frame
+			Event event;
+			while (w->popEvent(&event))
+			{
+				// If the application did not catch a close event, close now
+				if (event.Type == Event::EVENT_CLOSED)
+				{
+					running = false;
+				}
+			}
+
+			if (!running)
+			{
+				break;
+			}
+
+			GLfloat vertices[] = 
+			{
+				0.0f, 0.5f, 0.0f, -0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f,
+			};
+
+			glViewport(0, 0, 1600, 900);
+			glClearColor(0.0, 0, 0, 0);
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			// Use the program object
+			glUseProgram(mProgram);
+
+			// Load the vertex data
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vertices);
+			glEnableVertexAttribArray(0);
+
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+
+			egl->swap();
+			w->messageLoop();
+		}
+
+		w->destroy();
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
  }
 
 
